6dd1179bb4e2921ba47c1e2b6ed10b650af3259a
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-03-04 13:57:29 +0100

    v2

--- packet_implem.c
+++ packet_implem.c
@@ -17,16 +17,6 @@ struct __attribute__((__packed__)) pkt {
     uint32_t crc2;
 };
 
-struct __attribute__((__packed__)) ack_nack_pkt {
-    ptypes_t type : 2;
-    unsigned tr : 1;
-    unsigned window : 5;
-    uint8_t seqnum;
-    uint32_t timestamp;
-    uint32_t crc1;
-};
-typedef struct ack_nack_pkt ack_nack_pkt_t;
-
 /* Extra code */
 int is_ack_nack_pkt(const pkt_t* pkt)
 {
@@ -77,42 +67,27 @@ uint8_t pkt_get_window(const pkt_t* pkt)
 
 uint8_t pkt_get_seqnum(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        return ack_nack_pkt->seqnum;
-    }
     return pkt->seqnum;
 }
 
 uint16_t pkt_get_length(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        return 0;
-    }
-    return ntohl(pkt->length);
+    return pkt->length;
 }
 
 uint32_t pkt_get_timestamp(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        return ack_nack_pkt->timestamp;
-    }
     return pkt->timestamp;
 }
 
 uint32_t pkt_get_crc1(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        return ack_nack_pkt->crc1;
-    }
     return pkt->crc1;
 }
 
 const char* pkt_get_payload(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
+    if (pkt->length == 0) {
         return NULL;
     }
     return pkt->payload;
@@ -120,14 +95,10 @@ const char* pkt_get_payload(const pkt_t* pkt)
 
 uint32_t pkt_get_crc2(const pkt_t* pkt)
 {
-    if (is_ack_nack_pkt(pkt)) {
+    if (pkt->length == 0) {
         return 0;
     }
-
-    uint32_t crc2;
-    memcpy(&crc2, pkt + predict_header_length(pkt) + pkt_get_length(pkt), sizeof(uint32_t));
-
-    return crc2;
+    return pkt->crc2;
 }
 
 pkt_status_code pkt_set_type(pkt_t* pkt, const ptypes_t type)
@@ -154,52 +125,31 @@ pkt_status_code pkt_set_window(pkt_t* pkt, const uint8_t window)
 
 pkt_status_code pkt_set_seqnum(pkt_t* pkt, const uint8_t seqnum)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        ack_nack_pkt->seqnum = seqnum;
-        return PKT_OK;
-    }
     pkt->seqnum = seqnum;
     return PKT_OK;
 }
 
 pkt_status_code pkt_set_length(pkt_t* pkt, const uint16_t length)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        return E_LENGTH;
-    }
     pkt->length = length;
     return PKT_OK;
 }
 
 pkt_status_code pkt_set_timestamp(pkt_t* pkt, const uint32_t timestamp)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        ack_nack_pkt->timestamp = timestamp;
-        return PKT_OK;
-    }
     pkt->timestamp = timestamp;
     return PKT_OK;
 }
 
 pkt_status_code pkt_set_crc1(pkt_t* pkt, const uint32_t crc1)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        ack_nack_pkt_t* ack_nack_pkt = (ack_nack_pkt_t*)pkt;
-        ack_nack_pkt->crc1 = crc1;
-        return PKT_OK;
-    }
     pkt->crc1 = crc1;
     return PKT_OK;
 }
 
 pkt_status_code pkt_set_crc2(pkt_t* pkt, const uint32_t crc2)
 {
-    if (is_ack_nack_pkt(pkt)) {
-        return E_CRC;
-    }
-    memcpy(pkt + predict_header_length(pkt) + pkt_get_length(pkt), &crc2, sizeof(uint32_t));
+    pkt->crc2 = crc2;
     return PKT_OK;
 }
 
@@ -207,9 +157,12 @@ pkt_status_code pkt_set_payload(pkt_t* pkt,
     const char* data,
     const uint16_t length)
 {
-    pkt_set_length(pkt, length);
+    pkt_status_code result = pkt_set_length(pkt, length);
+    if (result != PKT_OK) {
+        return result;
+    }
 
-    memcpy(pkt + predict_header_length(pkt), data, length);
+    memcpy(pkt->payload, data, length);
 
     return PKT_OK;
 }
