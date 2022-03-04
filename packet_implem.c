031d68903aed0c0c849359158d9a0ae9f284e8f7
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2022-03-04 11:49:51 +0100

    Vscode formatting

--- packet_implem.c
+++ packet_implem.c
@@ -15,37 +15,37 @@ pkt_t* pkt_new()
     /* Your code will be inserted here */
 }
 
-void pkt_del(pkt_t *pkt)
+void pkt_del(pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
+pkt_status_code pkt_decode(const char* data, const size_t len, pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
+pkt_status_code pkt_encode(const pkt_t* pkt, char* buf, size_t* len)
 {
     /* Your code will be inserted here */
 }
 
-ptypes_t pkt_get_type  (const pkt_t* pkt)
+ptypes_t pkt_get_type(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-uint8_t  pkt_get_tr(const pkt_t* pkt)
+uint8_t pkt_get_tr(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-uint8_t  pkt_get_window(const pkt_t* pkt)
+uint8_t pkt_get_window(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-uint8_t  pkt_get_seqnum(const pkt_t* pkt)
+uint8_t pkt_get_seqnum(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
@@ -55,17 +55,17 @@ uint16_t pkt_get_length(const pkt_t* pkt)
     /* Your code will be inserted here */
 }
 
-uint32_t pkt_get_timestamp   (const pkt_t* pkt)
+uint32_t pkt_get_timestamp(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-uint32_t pkt_get_crc1   (const pkt_t* pkt)
+uint32_t pkt_get_crc1(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
 
-uint32_t pkt_get_crc2   (const pkt_t* pkt)
+uint32_t pkt_get_crc2(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
@@ -75,56 +75,54 @@ const char* pkt_get_payload(const pkt_t* pkt)
     /* Your code will be inserted here */
 }
 
-
-pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
+pkt_status_code pkt_set_type(pkt_t* pkt, const ptypes_t type)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_tr(pkt_t *pkt, const uint8_t tr)
+pkt_status_code pkt_set_tr(pkt_t* pkt, const uint8_t tr)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_window(pkt_t *pkt, const uint8_t window)
+pkt_status_code pkt_set_window(pkt_t* pkt, const uint8_t window)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum)
+pkt_status_code pkt_set_seqnum(pkt_t* pkt, const uint8_t seqnum)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length)
+pkt_status_code pkt_set_length(pkt_t* pkt, const uint16_t length)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_timestamp(pkt_t *pkt, const uint32_t timestamp)
+pkt_status_code pkt_set_timestamp(pkt_t* pkt, const uint32_t timestamp)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_crc1(pkt_t *pkt, const uint32_t crc1)
+pkt_status_code pkt_set_crc1(pkt_t* pkt, const uint32_t crc1)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_crc2(pkt_t *pkt, const uint32_t crc2)
+pkt_status_code pkt_set_crc2(pkt_t* pkt, const uint32_t crc2)
 {
     /* Your code will be inserted here */
 }
 
-pkt_status_code pkt_set_payload(pkt_t *pkt,
-                                const char *data,
-                                const uint16_t length)
+pkt_status_code pkt_set_payload(pkt_t* pkt,
+    const char* data,
+    const uint16_t length)
 {
     /* Your code will be inserted here */
 }
 
-
-ssize_t predict_header_length(const pkt_t *pkt)
+ssize_t predict_header_length(const pkt_t* pkt)
 {
     /* Your code will be inserted here */
 }
--- packet_interface.h
+++ packet_interface.h
@@ -3,15 +3,15 @@
 
 #include <stddef.h> /* size_t */
 #include <stdint.h> /* uintx_t */
+#include <stdio.h> /* ssize_t */
 #include <unistd.h>
-#include <stdio.h>  /* ssize_t */
 
 /* Taille maximale permise pour le payload */
 #define MAX_PAYLOAD_SIZE 512
 /* Taille maximale de Window */
 #define MAX_WINDOW_SIZE 31
 
-#define member_size(type, member) sizeof((((type *) NULL)->member))
+#define member_size(type, member) sizeof((((type*)NULL)->member))
 
 #define PKT_MAX_HEADERLEN offsetof(pkt_t, payload)
 #define PKT_MIN_HEADERLEN PKT_MAX_HEADERLEN - 2
@@ -28,25 +28,24 @@ typedef struct pkt pkt_t;
 
 /* Types de paquets */
 typedef enum {
-	PTYPE_FEC = 0,
-	PTYPE_DATA = 1,
-	PTYPE_ACK = 2,
-	PTYPE_NACK = 3,
+    PTYPE_FEC = 0,
+    PTYPE_DATA = 1,
+    PTYPE_ACK = 2,
+    PTYPE_NACK = 3,
 } ptypes_t;
-
 
 /* Valeur de retours des fonctions */
 typedef enum {
-	PKT_OK = 0,     /* Le paquet a été traité avec succès */
-	E_TYPE,         /* Erreur liée au champs Type */
-	E_TR,           /* Erreur liee au champ TR */
-	E_LENGTH,       /* Erreur liée au champs Length  */
-	E_CRC,          /* CRC invalide */
-	E_WINDOW,       /* Erreur liée au champs Window */
-	E_SEQNUM,       /* Numéro de séquence invalide */
-	E_NOMEM,        /* Pas assez de mémoire */
-	E_NOHEADER,     /* Le paquet n'a pas de header (trop court) */
-	E_UNCONSISTENT, /* Le paquet est incohérent */
+    PKT_OK = 0, /* Le paquet a été traité avec succès */
+    E_TYPE, /* Erreur liée au champs Type */
+    E_TR, /* Erreur liee au champ TR */
+    E_LENGTH, /* Erreur liée au champs Length  */
+    E_CRC, /* CRC invalide */
+    E_WINDOW, /* Erreur liée au champs Window */
+    E_SEQNUM, /* Numéro de séquence invalide */
+    E_NOMEM, /* Pas assez de mémoire */
+    E_NOHEADER, /* Le paquet n'a pas de header (trop court) */
+    E_UNCONSISTENT, /* Le paquet est incohérent */
 } pkt_status_code;
 
 /* Alloue et initialise une struct pkt
@@ -59,7 +58,7 @@ void pkt_del(pkt_t*);
 /*
  * Retourne la longueur du header en bytes.
  */
-ssize_t predict_header_length(const pkt_t *pkt);
+ssize_t predict_header_length(const pkt_t* pkt);
 
 /*
  * Décode des données reçues et crée une nouvelle structure pkt.
@@ -81,7 +80,7 @@ ssize_t predict_header_length(const pkt_t *pkt);
  * @return: Un code indiquant si l'opération a réussi ou représentant
  *         l'erreur rencontrée.
  */
-pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt);
+pkt_status_code pkt_decode(const char* data, const size_t len, pkt_t* pkt);
 
 /*
  * Encode une struct pkt dans un buffer, prêt à être envoyé sur le réseau
@@ -97,19 +96,19 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt);
  * @return: Un code indiquant si l'opération a réussi ou E_NOMEM si
  *         le buffer est trop petit.
  */
-pkt_status_code pkt_encode(const pkt_t*, char *buf, size_t *len);
+pkt_status_code pkt_encode(const pkt_t*, char* buf, size_t* len);
 
 /* Accesseurs pour les champs toujours présents du paquet.
  * Les valeurs renvoyées sont toutes dans l'endianness native
  * de la machine!
  */
-ptypes_t pkt_get_type     (const pkt_t*);
-uint8_t  pkt_get_tr       (const pkt_t*);
-uint8_t  pkt_get_window   (const pkt_t*);
-uint8_t  pkt_get_seqnum   (const pkt_t*);
-uint16_t pkt_get_length   (const pkt_t*);
+ptypes_t pkt_get_type(const pkt_t*);
+uint8_t pkt_get_tr(const pkt_t*);
+uint8_t pkt_get_window(const pkt_t*);
+uint8_t pkt_get_seqnum(const pkt_t*);
+uint16_t pkt_get_length(const pkt_t*);
 uint32_t pkt_get_timestamp(const pkt_t*);
-uint32_t pkt_get_crc1      (const pkt_t*);
+uint32_t pkt_get_crc1(const pkt_t*);
 /* Renvoie un pointeur vers le payload du paquet, ou NULL s'il n'y
  * en a pas.
  */
@@ -124,24 +123,23 @@ uint32_t pkt_get_crc2(const pkt_t*);
  * doivent renvoyer un code d'erreur adapté.
  * Les valeurs fournies sont dans l'endianness native de la machine!
  */
-pkt_status_code pkt_set_type     (pkt_t*, const ptypes_t type);
-pkt_status_code pkt_set_tr       (pkt_t*, const uint8_t tr);
-pkt_status_code pkt_set_window   (pkt_t*, const uint8_t window);
-pkt_status_code pkt_set_seqnum   (pkt_t*, const uint8_t seqnum);
-pkt_status_code pkt_set_length   (pkt_t*, const uint16_t length);
+pkt_status_code pkt_set_type(pkt_t*, const ptypes_t type);
+pkt_status_code pkt_set_tr(pkt_t*, const uint8_t tr);
+pkt_status_code pkt_set_window(pkt_t*, const uint8_t window);
+pkt_status_code pkt_set_seqnum(pkt_t*, const uint8_t seqnum);
+pkt_status_code pkt_set_length(pkt_t*, const uint16_t length);
 pkt_status_code pkt_set_timestamp(pkt_t*, const uint32_t timestamp);
-pkt_status_code pkt_set_crc1      (pkt_t*, const uint32_t crc1);
+pkt_status_code pkt_set_crc1(pkt_t*, const uint32_t crc1);
 /* Défini la valeur du champs payload du paquet.
  * @data: Une succession d'octets représentants le payload
  * @length: Le nombre d'octets composant le payload
  * @POST: pkt_get_length(pkt) == length */
 pkt_status_code pkt_set_payload(pkt_t*,
-                                const char *data,
-                                const uint16_t length);
+    const char* data,
+    const uint16_t length);
 /* Setter pour CRC2. Les valeurs fournies sont dans l'endianness
  * native de la machine!
  */
 pkt_status_code pkt_set_crc2(pkt_t*, const uint32_t crc2);
 
-
-#endif  /* __PACKET_INTERFACE_H_ */
+#endif /* __PACKET_INTERFACE_H_ */
